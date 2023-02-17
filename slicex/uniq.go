271643df08e4d97416871b356cf71f75d110e828
soyacen <soyacen@outlook.com>
2023-02-17 13:21:18 +0800

    fix uniq

--- slicex/uniq.go
+++ slicex/uniq.go
@@ -1,14 +1,25 @@
 package slicex
 
+import "golang.org/x/exp/slices"
+
 func Uniq[S ~[]E, E comparable](s S) S {
 	if s == nil {
 		return nil
 	}
-	m := make(map[E]struct{}, len(s))
+	length := len(s)
+	r := make(S, 0, length)
+	if length <= 8 {
+		for _, v := range s {
+			if !slices.Contains(r, v) {
+				r = append(r, v)
+			}
+		}
+		return r
+	}
+	m := make(map[E]struct{}, length)
 	for _, v := range s {
 		m[v] = struct{}{}
 	}
-	r := make(S, 0, len(m))
 	for k := range m {
 		r = append(r, k)
 	}
