3ea44b0ea12f65956794007432b6351ed8fa76f5
soyacen <soyacen@outlook.com>
2023-02-20 15:27:45 +0800

    rm duplicate method

--- syncx/chanx/to_receive_channels.go
+++
@@ -1,9 +0,0 @@
-package chanx
-
-func ToReceiveChannels[T any](channels ...chan T) []chan<- T {
-	c := make([]chan<- T, len(channels))
-	for _, ch := range channels {
-		c = append(c, ch)
-	}
-	return c
-}
