e1475a2a2e27b2b47ecd332a8cebab31da7c5786
soyacen <soyacen@outlook.com>
2023-02-20 11:14:41 +0800

    update go version to go 1.20

--- contextx/context.go
+++ contextx/context.go
@@ -2,24 +2,27 @@ package contextx
 
 import (
 	"context"
+	"fmt"
 	"os"
 	"os/signal"
 )
 
 // Signal creates a new context that cancels on the given signals.
-func Signal(signals ...os.Signal) (context.Context, context.CancelFunc) {
+func Signal(signals ...os.Signal) (context.Context, context.CancelCauseFunc) {
 	return WithSignal(context.Background(), signals...)
 }
 
 // WithSignal creates a new context that cancels on the given signals.
-func WithSignal(ctx context.Context, signals ...os.Signal) (context.Context, context.CancelFunc) {
-	ctx, cancelFunc := context.WithCancel(ctx)
+func WithSignal(ctx context.Context, signals ...os.Signal) (context.Context, context.CancelCauseFunc) {
+	ctx, cancelFunc := context.WithCancelCause(ctx)
 	signalC := make(chan os.Signal, 1)
+	defer close(signalC)
 	signal.Notify(signalC, signals...)
+	defer signal.Stop(signalC)
 	go func() {
 		select {
-		case <-signalC:
-			cancelFunc()
+		case incomingSignal := <-signalC:
+			cancelFunc(fmt.Errorf("receive signal, %s", incomingSignal))
 		case <-ctx.Done():
 			signal.Stop(signalC)
 		}
--- go.mod
+++ go.mod
@@ -1,6 +1,6 @@
 module github.com/go-leo/gox
 
-go 1.19
+go 1.20
 
 require (
 	github.com/spf13/cast v1.5.0
