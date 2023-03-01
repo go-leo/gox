206c7f4e01b844bf71fab4192129a712c6e41b1f
soyacen <soyacen@outlook.com>
2023-03-01 15:56:45 +0800

    add runtimex.Stack method

--- runtimex/stack.go
+++ runtimex/stack.go
@@ -1,4 +1,4 @@
-package errorx
+package runtimex
 
 import (
 	"bytes"
@@ -15,6 +15,8 @@ var (
 )
 
 // Stack returns a nicely formatted stack frame, skipping skip frames.
+// From github.com/gin-gonic/gin/recovery.go
+// Copyright (c) 2014 Manuel Martínez-Almeida
 func Stack(skip int) []byte {
 	buf := new(bytes.Buffer) // the returned data
 	// As we loop, we open files and read them. These variables record the currently
@@ -27,7 +29,7 @@ func Stack(skip int) []byte {
 			break
 		}
 		// Print this much at least.  If we can't find the source, it won't show.
-		fmt.Fprintf(buf, "%s:%d (0x%x)\n", file, line, pc)
+		_, _ = fmt.Fprintf(buf, "%s:%d (0x%x)\n", file, line, pc)
 		if file != lastFile {
 			data, err := os.ReadFile(file)
 			if err != nil {
@@ -36,7 +38,7 @@ func Stack(skip int) []byte {
 			lines = bytes.Split(data, []byte{'\n'})
 			lastFile = file
 		}
-		fmt.Fprintf(buf, "\t%s: %s\n", function(pc), source(lines, line))
+		_, _ = fmt.Fprintf(buf, "\t%s: %s\n", function(pc), source(lines, line))
 	}
 	return buf.Bytes()
 }
