7ce98b3d9261f1cfc08979f21a16aa59961cd790
soyacen <soyacen@outlook.com>
2023-03-28 17:19:45 +0800

    add bytex pool

---
+++ bytex/pool_test.go
@@ -0,0 +1,28 @@
+package bytex
+
+import (
+	"runtime"
+	"runtime/debug"
+	"testing"
+)
+
+func TestPool(t *testing.T) {
+	defer debug.SetGCPercent(debug.SetGCPercent(-1))
+	var p Pool
+
+	buf := p.Get()
+	t.Logf("%p", buf)
+	p.Put(buf)
+	buf = nil
+
+	newBuf := p.Get()
+	t.Logf("%p", newBuf)
+	newBuf.Free()
+	newBuf = nil
+
+	runtime.GC()
+
+	lastBuf := p.Get()
+	t.Logf("%p", lastBuf)
+	p.Put(lastBuf)
+}
