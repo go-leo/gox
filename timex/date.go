1a9d4401af90a12682d8bd244b9d141a6f7c2808
soyacen <soyacen@outlook.com>
2023-05-05 09:42:58 +0800

    fix bug

--- timex/date.go
+++ timex/date.go
@@ -10,10 +10,22 @@ func Today() time.Time {
 	return Date(time.Now())
 }
 
-func Tomorrow() time.Time {
-	return Today().AddDate(0, 0, 1)
+func Tomorrow(ts ...time.Time) time.Time {
+	if len(ts) <= 0 {
+		return Today().AddDate(0, 0, 1)
+	}
+	if ts[0].IsZero() {
+		return Today().AddDate(0, 0, 1)
+	}
+	return ts[0].AddDate(0, 0, 1)
 }
 
-func Yesterday() time.Time {
-	return Today().AddDate(0, 0, -1)
+func Yesterday(ts ...time.Time) time.Time {
+	if len(ts) <= 0 {
+		return Today().AddDate(0, 0, -1)
+	}
+	if ts[0].IsZero() {
+		return Today().AddDate(0, 0, -1)
+	}
+	return ts[0].AddDate(0, 0, -1)
 }
