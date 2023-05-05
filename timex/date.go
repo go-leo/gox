f405462b9fe8d1ac89c99518e35466de7b4c7ee2
soyacen <soyacen@outlook.com>
2023-05-05 09:37:40 +0800

    fix Today Tomorrow and Yesterday

--- timex/date.go
+++ timex/date.go
@@ -5,3 +5,15 @@ import "time"
 func Date(t time.Time) time.Time {
 	return time.Date(t.Year(), t.Month(), t.Day(), 0, 0, 0, 0, t.Location())
 }
+
+func Today() time.Time {
+	return Date(time.Now())
+}
+
+func Tomorrow() time.Time {
+	return Today().AddDate(0, 0, 1)
+}
+
+func Yesterday() time.Time {
+	return Today().AddDate(0, 0, -1)
+}
