1c138d6cc06aba0ededfa91bf45a64e6300b83cb
soyacen <soyacen@outlook.com>
2023-05-17 21:00:55 +0800

    add randx Shuffle

--- mathx/randx/numeric.go
+++ mathx/randx/numeric.go
@@ -131,31 +131,3 @@ func Float32() float32 {
 	Put(r)
 	return f
 }
-
-func PickItemsInt32(size int, n ...int32) []int32 {
-	if size <= 0 {
-		return []int32{}
-	}
-	copy := append([]int32{}, n...)
-	if size >= len(n) {
-		return copy
-	}
-	indexSet := make(map[int]struct{})
-	for i := 0; i < size; i++ {
-		index := Intn(len(n))
-		if _, ok := indexSet[index]; !ok {
-			indexSet[index] = struct{}{}
-			continue
-		}
-		for {
-			index = Intn(len(n))
-			if _, ok := indexSet[index]; !ok {
-				indexSet[index] = struct{}{}
-				break
-			}
-		}
-
-	}
-
-	return copy[:size]
-}
