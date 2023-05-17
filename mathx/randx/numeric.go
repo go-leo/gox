599360624f4671eeef90992aa72f6dbe839bb68d
soyacen <soyacen@outlook.com>
2023-05-17 20:44:00 +0800

    add uint randx

--- mathx/randx/numeric.go
+++ mathx/randx/numeric.go
@@ -1,5 +1,7 @@
 package randx
 
+import "math"
+
 // Int63 returns a non-negative pseudo-random 63-bit integer as an int64.
 func Int63() int64 {
 	r := Get()
@@ -32,14 +34,6 @@ func Int31() int32 {
 	return i
 }
 
-// Int returns a non-negative pseudo-random int.
-func Int() int {
-	r := Get()
-	i := r.Int()
-	Put(r)
-	return i
-}
-
 // Int63n returns, as an int64, a non-negative pseudo-random number in the half-open interval [0,n).
 // It panics if n <= 0.
 func Int63n(n int64) int64 {
@@ -58,6 +52,34 @@ func Int31n(n int32) int32 {
 	return i
 }
 
+// Int63Range returns a non-negative pseudo-random 63-bit integer as an int64 between min and max.
+func Int63Range(min, max int64) int64 {
+	return Int63n(max-min) + min
+}
+
+// Uint32Range returns a pseudo-random 32-bit value as a uint32 between min and max.
+func Uint32Range(min, max uint32) uint32 {
+	return (Uint32() % (max - min)) * min
+}
+
+// Uint64Range returns a pseudo-random 64-bit value as a uint64 between min and max.
+func Uint64Range(min, max uint64) uint64 {
+	return (Uint64() % (max - min)) * min
+}
+
+// Int31Range returns a non-negative pseudo-random 31-bit integer as an int32 between min and max.
+func Int31Range(min, max int32) int32 {
+	return Int31n(max-min) + min
+}
+
+// Int returns a non-negative pseudo-random int.
+func Int() int {
+	r := Get()
+	i := r.Int()
+	Put(r)
+	return i
+}
+
 // Intn returns, as an int, a non-negative pseudo-random number in the half-open interval [0,n).
 // It panics if n <= 0.
 func Intn(n int) int {
@@ -65,6 +87,33 @@ func Intn(n int) int {
 	i := r.Intn(n)
 	Put(r)
 	return i
+}
+
+// IntRange returns a non-negative pseudo-random int between min and max.
+func IntRange(min, max int) int {
+	return Intn(max-min) + min
+}
+
+// Uint returns a non-negative pseudo-random uint.
+func Uint() uint {
+	return uint(Uint64())
+}
+
+// Uintn returns, as an uint, a non-negative pseudo-random number in the half-open interval [0,n).
+// It panics if n <= 0.
+func Uintn(n uint) uint {
+	if n <= 0 {
+		panic("invalid argument to Uintn")
+	}
+	if n <= math.MaxUint32 {
+		return uint(Uint32()) % n
+	}
+	return uint(Uint64()) % n
+}
+
+// UintRange returns a non-negative pseudo-random int between min and max.
+func UintRange(min, max uint) uint {
+	return Uintn(max-min) + min
 }
 
 // Float64 returns, as a float64, a pseudo-random number in the half-open interval [0.0,1.0).
