e93f2ba3230a425008081bee31b6920b52ef2da4
soyacen <soyacen@outlook.com>
2023-10-23 19:34:12 +0800

    feat: add contextx Signalxxx method

--- contextx/signal.go
+++ contextx/signal.go
@@ -1,9 +1,8 @@
-//go:build !go1.20
-
 package contextx
 
 import (
 	"context"
+	"fmt"
 	"os"
 	"os/signal"
 )
@@ -15,5 +14,60 @@ func Signal(signals ...os.Signal) (context.Context, context.CancelFunc) {
 
 // WithSignal like signal.NotifyContext.
 func WithSignal(ctx context.Context, signals ...os.Signal) (context.Context, context.CancelFunc) {
-	return signal.NotifyContext(ctx, signals...)
+	newCtx, cancelFunc := context.WithCancel(ctx)
+	if Error(ctx) != nil {
+		return newCtx, cancelFunc
+	}
+	go func() {
+		signalC := make(chan os.Signal, 1)
+		defer close(signalC)
+
+		signal.Notify(signalC, signals...)
+		defer signal.Stop(signalC)
+
+		select {
+		case <-signalC:
+			cancelFunc()
+			return
+		case <-newCtx.Done():
+			return
+		}
+	}()
+	return newCtx, cancelFunc
+}
+
+type SignalError struct {
+	Signal os.Signal
+}
+
+func (e SignalError) Error() string {
+	return fmt.Sprintf("received [%s] signal", e.Signal)
+}
+
+// SignalCause creates a new context that cancels on the given signals.
+func SignalCause(signals ...os.Signal) (context.Context, context.CancelCauseFunc) {
+	return WithSignalCause(context.Background(), signals...)
+}
+
+func WithSignalCause(ctx context.Context, signals ...os.Signal) (context.Context, context.CancelCauseFunc) {
+	newCtx, cancelFunc := context.WithCancelCause(ctx)
+	if Error(ctx) != nil {
+		return newCtx, cancelFunc
+	}
+	go func() {
+		signalC := make(chan os.Signal, 1)
+		defer close(signalC)
+
+		signal.Notify(signalC, signals...)
+		defer signal.Stop(signalC)
+
+		select {
+		case sig := <-signalC:
+			cancelFunc(SignalError{Signal: sig})
+			return
+		case <-newCtx.Done():
+			return
+		}
+	}()
+	return newCtx, cancelFunc
 }
