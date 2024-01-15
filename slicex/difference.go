f5beef5cd5e2d221d614213886b11be10ffa2cc1
soyacen <soyacen@outlook.com>
2024-01-15 21:00:59 +0800

    feat: add slicex.difference

--- slicex/difference.go
+++ slicex/difference.go
@@ -4,9 +4,16 @@ import "golang.org/x/exp/slices"
 
 // Difference 返回差集
 func Difference[S ~[]E, E comparable](s1 S, s2 S) S {
+	if len(s1) >= len(s2) {
+		return difference(s1, s2)
+	}
+	return difference(s2, s1)
+}
+
+func difference[S ~[]E, E comparable](a S, b S) S {
 	var r S
-	for _, v := range s1 {
-		if !slices.Contains(s2, v) {
+	for _, v := range a {
+		if !slices.Contains(b, v) {
 			r = append(r, v)
 		}
 	}
