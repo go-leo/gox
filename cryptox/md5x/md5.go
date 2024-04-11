3378e8edf15ea7c98cde2b177ca8d7ebd0565116
soyacen <soyacen@outlook.com>
2024-04-11 17:49:34 +0800

    feat: add md5x.TextMD5 and TextMD5Hex

--- cryptox/md5x/md5.go
+++ cryptox/md5x/md5.go
@@ -3,6 +3,7 @@ package md5x
 import (
 	"crypto/md5"
 	"encoding/hex"
+	"io"
 )
 
 func MD5(data []byte) []byte {
@@ -13,4 +14,14 @@ func MD5(data []byte) []byte {
 
 func MD5Hex(data []byte) string {
 	return hex.EncodeToString(MD5(data))
+}
+
+func TextMD5(text string) []byte {
+	hash := md5.New()
+	_, _ = io.WriteString(hash, text)
+	return hash.Sum(nil)
+}
+
+func TextMD5Hex(text string) string {
+	return hex.EncodeToString(TextMD5(text))
 }
