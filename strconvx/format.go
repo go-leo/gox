671d3c0f3ff1949e32cf0f18d189a7998888d119
soyacen <soyacen@outlook.com>
2024-05-16 14:39:27 +0800

    feat: add strconvx.FormatXXX

---
+++ strconvx/format.go
@@ -0,0 +1,54 @@
+package strconvx
+
+import (
+	"golang.org/x/exp/constraints"
+	"strconv"
+)
+
+func FormatBool(b bool) string {
+	return strconv.FormatBool(b)
+}
+
+func FormatUint[Unsigned constraints.Unsigned](i Unsigned, base int) string {
+	return strconv.FormatUint(uint64(i), base)
+}
+
+func FormatInt[Signed constraints.Signed](i Signed, base int) string {
+	return strconv.FormatInt(int64(i), base)
+}
+
+func FormatFloat[Float constraints.Float](f Float, fmt byte, prec, bitSize int) string {
+	return strconv.FormatFloat(float64(f), fmt, prec, bitSize)
+}
+
+func FormatBoolSlice(s []bool) []string {
+	r := make([]string, 0, len(s))
+	for _, b := range s {
+		r = append(r, FormatBool(b))
+	}
+	return r
+}
+
+func FormatUintSlice[Unsigned constraints.Unsigned](s []Unsigned, base int) []string {
+	r := make([]string, 0, len(s))
+	for _, i := range s {
+		r = append(r, FormatUint(i, base))
+	}
+	return r
+}
+
+func FormatIntSlice[Signed constraints.Signed](s []Signed, base int) []string {
+	r := make([]string, 0, len(s))
+	for _, i := range s {
+		r = append(r, FormatInt(i, base))
+	}
+	return r
+}
+
+func FormatFloatSlice[Float constraints.Float](s []Float, fmt byte, prec, bitSize int) []string {
+	r := make([]string, 0, len(s))
+	for _, f := range s {
+		r = append(r, FormatFloat(float64(f), fmt, prec, bitSize))
+	}
+	return r
+}
