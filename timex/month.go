d5e0158dce22879716414c85ca9fb89095b61019
soyacen <soyacen@outlook.com>
2024-05-20 09:29:42 +0800

    feat: add CurrentMonth

--- timex/month.go
+++ timex/month.go
@@ -1,17 +1,20 @@
 package timex
 
-import "time"
+import (
+	"time"
+)
 
 func Month(t time.Time) time.Time {
 	return time.Date(t.Year(), t.Month(), 1, 0, 0, 0, 0, t.Location())
 }
 
+func CurrentMonth() time.Time {
+	return Month(time.Now())
+}
+
 func ThisMonth(ts ...time.Time) time.Time {
 	if len(ts) <= 0 {
-		return Month(time.Now())
-	}
-	if ts[0].IsZero() {
-		return Month(time.Now())
+		return CurrentMonth()
 	}
 	return Month(ts[0])
 }
