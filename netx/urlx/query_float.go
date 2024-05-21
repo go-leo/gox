f5ad4f5687935e47ee4de23f7ab292df41ceee0c
soyacen <soyacen@outlook.com>
2024-05-21 10:20:16 +0800

    feat: add urlx.GetFloatxxx

---
+++ netx/urlx/query_float.go
@@ -0,0 +1,56 @@
+package urlx
+
+import (
+	"github.com/go-leo/gox/protox"
+	"github.com/go-leo/gox/strconvx"
+	"golang.org/x/exp/constraints"
+	"google.golang.org/protobuf/types/known/wrapperspb"
+	"net/url"
+)
+
+func GetFloat[Float constraints.Float](queries url.Values, key string) func() (Float, error) {
+	return func() (Float, error) {
+		return strconvx.ParseFloat[Float](queries.Get(key), 64)
+	}
+}
+
+func GetFloatPtr[Float constraints.Float](queries url.Values, key string) func() (*Float, error) {
+	return func() (*Float, error) {
+		v, err := GetFloat[Float](queries, key)()
+		return &v, err
+	}
+}
+
+func GetFloatSlice[Float constraints.Float](queries url.Values, key string) func() ([]Float, error) {
+	return func() ([]Float, error) {
+		return strconvx.ParseFloatSlice[Float](queries[key], 64)
+	}
+}
+
+func GetFloat32Value(queries url.Values, key string) func() (*wrapperspb.FloatValue, error) {
+	return func() (*wrapperspb.FloatValue, error) {
+		v, err := GetFloat[float32](queries, key)()
+		return wrapperspb.Float(v), err
+	}
+}
+
+func GetFloat32ValueSlice(queries url.Values, key string) func() ([]*wrapperspb.FloatValue, error) {
+	return func() ([]*wrapperspb.FloatValue, error) {
+		v, err := GetFloatSlice[float32](queries, key)()
+		return protox.WrapFloat32Slice(v), err
+	}
+}
+
+func GetFloat64Value(queries url.Values, key string) func() (*wrapperspb.DoubleValue, error) {
+	return func() (*wrapperspb.DoubleValue, error) {
+		v, err := GetFloat[float64](queries, key)()
+		return wrapperspb.Double(v), err
+	}
+}
+
+func GetFloat64ValueSlice(queries url.Values, key string) func() ([]*wrapperspb.DoubleValue, error) {
+	return func() ([]*wrapperspb.DoubleValue, error) {
+		v, err := GetFloatSlice[float64](queries, key)()
+		return protox.WrapFloat64Slice(v), err
+	}
+}
