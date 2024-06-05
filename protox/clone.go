a856a3387ef727ccc4ecb6ac160827fd5d373286
soyacen <soyacen@outlook.com>
2024-06-05 14:10:08 +0800

    feat: check m if is nil

--- protox/clone.go
+++ protox/clone.go
@@ -3,5 +3,10 @@ package protox
 import "google.golang.org/protobuf/proto"
 
 func Clone[M proto.Message](m M) M {
-	return proto.Clone(m).(M)
+	var cloned M
+	if m == nil {
+		return cloned
+	}
+	cloned = proto.Clone(m).(M)
+	return cloned
 }
