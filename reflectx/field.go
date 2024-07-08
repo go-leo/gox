cf3e4ff04c953e9613a2b94495f4a7416343b11b
soyacen <soyacen@outlook.com>
2024-07-08 11:30:47 +0800

    feat: add reflectx.FindFieldByTag

--- reflectx/field.go
+++ reflectx/field.go
@@ -4,14 +4,19 @@ import "reflect"
 
 // FindFieldByTag searches through the fields of a given struct value for a field
 // that has a specific tag and whose tag value satisfies a given condition.
-// It returns the reflect.Value of the matched field or a zero Value if no match is found.
+// It returns the reflect.Value of the matched field along with a boolean indicating
+// whether a match was found.
 //
 // Parameters:
 //
 //	v: The reflect.Value representing the struct to search.
 //	tagKey: The key of the tag to look for.
-//	equals: A function that takes the tag value as a string and returns true if it satisfies the condition.
-func FindFieldByTag(v reflect.Value, tagKey string, equals func(tagVal string) bool) (reflect.Value, bool) {
+//	match: A function that takes the tag value as a string and returns true if it satisfies the condition.
+//
+// Returns:
+//
+//	A tuple containing the reflect.Value of the matched field and a boolean indicating if a match was found.
+func FindFieldByTag(v reflect.Value, tagKey string, match func(tagVal string) bool) (reflect.Value, bool) {
 
 	// Indirect the value to get the underlying value.
 	value := IndirectValue(v)
@@ -31,7 +36,7 @@ func FindFieldByTag(v reflect.Value, tagKey string, equals func(tagVal string) b
 		structField := t.Field(i)
 
 		// Check if the field has the specified tag with the given value.
-		if tagVal, ok := structField.Tag.Lookup(tagKey); ok && equals(tagVal) {
+		if tagVal, ok := structField.Tag.Lookup(tagKey); ok && match(tagVal) {
 			// Return the field value if the tag matches.
 			return field, true
 		}
