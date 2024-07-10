5747d1f496283810004f9f287a20a1c27baef3e4
soyacen <soyacen@outlook.com>
2024-07-10 16:32:03 +0800

    feat: add chanx fan in and fan out pattern

--- syncx/chanx/fan_in.go
+++ syncx/chanx/fan_in.go
@@ -1,4 +1,12 @@
 package chanx
 
-// A function can read from multiple inputs and proceed until all are closed by multiplexing the input
-// channels onto a single channel that’s closed when all the inputs are closed. This is called fan-in.
+import (
+	"context"
+)
+
+// FanIn can read from multiple inputs and proceed until all are closed by multiplexing the input
+// channels onto a single channel that’s closed when all the inputs are closed.
+// See: [concurrency](https://go.dev/talks/2012/concurrency.slide#28)
+func FanIn[T any](ctx context.Context, channels ...<-chan T) <-chan T {
+	return Merge[T](ctx, channels...)
+}
--- syncx/chanx/fan_out.go
+++ syncx/chanx/fan_out.go
@@ -1,4 +1,9 @@
 package chanx
 
-// Multiple functions can read from the same channel until that channel is closed; this is called fan-out.
+import "context"
+
+// FanOut multiple functions can read from the same channel until that channel is closed; this is called fan-out.
 // This provides a way to distribute work amongst a group of workers to parallelize CPU use and I/O.
+func FanOut[T any, R any](ctx context.Context, src <-chan T, f func(T) R) chan<- R {
+	return Pipeline[T, R](ctx, src, f)
+}
