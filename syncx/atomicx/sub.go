29e0c7e0a250b71f930e6d5a565ec834d31a1027
soyacen <soyacen@outlook.com>
2024-07-25 14:06:54 +0800

    feat: add atomicx package

---
+++ syncx/atomicx/sub.go
@@ -0,0 +1,23 @@
+package atomicx
+
+import "sync/atomic"
+
+func SubUint32(addr *uint32, delta int32) (new uint32) {
+	return atomic.AddUint32(addr, ^uint32(delta-1))
+}
+
+func DecrUint32(addr *uint32) (new uint32) {
+	return SubUint32(addr, 1)
+}
+
+func SubUint64(addr *uint64, delta int64) (new uint64) {
+	return atomic.AddUint64(addr, ^uint64(delta-1))
+}
+
+func DecrUint64(addr *uint64) (new uint64) {
+	return SubUint64(addr, 1)
+}
+
+//func SubUintptr(addr *uintptr, delta uintptr) (new uintptr) {
+//	return atomic.AddUintptr(addr, ^(delta - 1))
+//}
