48399ae3ad11efb998b923961f7c2826f248376d
soyacen <soyacen@outlook.com>
2024-08-16 19:13:56 +0800

    feat: fix timex.Tomorrow and Yesterday

--- timex/date.go
+++ timex/date.go
@@ -17,21 +17,9 @@ func Today(ts ...time.Time) time.Time {
 }
 
 func Tomorrow(ts ...time.Time) time.Time {
-	if len(ts) <= 0 {
-		return Today().AddDate(0, 0, 1)
-	}
-	if ts[0].IsZero() {
-		return Today().AddDate(0, 0, 1)
-	}
-	return ts[0].AddDate(0, 0, 1)
+	return Today(ts...).AddDate(0, 0, 1)
 }
 
 func Yesterday(ts ...time.Time) time.Time {
-	if len(ts) <= 0 {
-		return Today().AddDate(0, 0, -1)
-	}
-	if ts[0].IsZero() {
-		return Today().AddDate(0, 0, -1)
-	}
-	return ts[0].AddDate(0, 0, -1)
+	return Today(ts...).AddDate(0, 0, -1)
 }
