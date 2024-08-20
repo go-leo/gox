d8822a2d0945485d59f1597c3e7e7155585f0d32
soyacen <soyacen@outlook.com>
2024-08-20 10:06:21 +0800

    feat: add limit and skip method

--- syncx/chanx/emit.go
+++ syncx/chanx/emit.go
@@ -2,18 +2,8 @@ package chanx
 
 import "context"
 
-// Emit 函数接收任意数量和类型的参数，创建一个同类型的通道，将这些值依次放入通道后返回，并最终关闭此通道。
-func Emit[T any](values ...T) <-chan T {
-	out := make(chan T, len(values))
-	for _, value := range values {
-		out <- value
-	}
-	close(out)
-	return out
-}
-
-// AsyncEmit 创建一个通道，异步发送传入的值，并允许通过上下文取消发送过程。
-func AsyncEmit[T any](ctx context.Context, values ...T) <-chan T {
+// Emit 创建一个通道，异步发送传入的值，并允许通过上下文取消发送过程。
+func Emit[T any](ctx context.Context, values ...T) <-chan T {
 	out := make(chan T)
 	go func() {
 		defer close(out)
--- syncx/chanx/reduce.go
+++ syncx/chanx/reduce.go
@@ -17,11 +17,12 @@ func Reduce[T any, R any](in <-chan T, identity R, accumulator func(R, T) R) R {
 }
 
 // AsyncReduce 异步处理通道数据，使用累积器函数累加元素，并能响应上下文取消操作，最终返回累积结果。
-func AsyncReduce[T any, R any](ctx context.Context, in <-chan T, identity R, accumulator func(context.Context, R, T) R) R {
+func AsyncReduce[T any, R any](ctx context.Context, in <-chan T, identity R, accumulator func(context.Context, R, T) R) <-chan R {
+	var out chan R
 	if in == nil {
-		return identity
+		return out
 	}
-	out := make(chan R)
+	out = make(chan R)
 	go func(identity R) {
 		defer close(out)
 		for {
@@ -37,5 +38,5 @@ func AsyncReduce[T any, R any](ctx context.Context, in <-chan T, identity R, acc
 			}
 		}
 	}(identity)
-	return <-out
+	return out
 }
