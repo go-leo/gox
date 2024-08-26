a5ae01fdb71e2e128d8add5318956c38f380dfad
soyacen <soyacen@outlook.com>
2024-08-26 13:43:42 +0800

    feat: add mapx.LoadOrCreate

--- mapx/function.go
+++ mapx/function.go
@@ -1,5 +1,9 @@
 package mapx
 
+import (
+	"golang.org/x/sync/singleflight"
+)
+
 func AppendValue[M ~map[K]V, R ~map[K]S, S ~[]V, K comparable, V any](r R, ms ...M) R {
 	if r == nil {
 		r = make(R)
@@ -43,4 +47,23 @@ func FromMapRange[M ~map[K]V, K comparable, V any](mi MapInterface) M {
 		return true
 	})
 	return m
+}
+
+// LoadOrCreate 尝试从给定的Map中加载键key的值，若不存在，则仅执行一次计算函数f并将结果存入Map，最后返回值及是否已存在的标志。
+func LoadOrCreate(m MapInterface, sfg *singleflight.Group, key string, f func() (any, error)) (any, error, bool) {
+	if value, ok := m.Load(key); ok {
+		return value, nil, true
+	}
+	value, err, _ := sfg.Do(key, func() (interface{}, error) {
+		if value, ok := m.Load(key); ok {
+			return value, nil
+		}
+		value, err := f()
+		if err != nil {
+			return nil, err
+		}
+		m.Store(key, value)
+		return value, nil
+	})
+	return value, err, false
 }
