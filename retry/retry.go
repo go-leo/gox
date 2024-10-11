07cf21c8e1d46d35442627935fba82f70b6bd14f
soyacen <soyacen@outlook.com>
2024-10-11 22:55:11 +0800

    feat: add MaxAttempts

--- retry/retry.go
+++ retry/retry.go
@@ -7,6 +7,55 @@ import (
 	"github.com/go-leo/gox/backoff"
 )
 
+type Executor interface {
+	Exec(ctx context.Context, cmd func(ctx context.Context, attempt uint) error) error
+}
+
+type Retry interface {
+	Backoff(backoffFunc backoff.BackoffFunc) Executor
+}
+
+type retryStrategy struct {
+	maxAttempts uint
+	backoffFunc backoff.BackoffFunc
+}
+
+
+// 实现WithBackoff接口中的Backoff方法
+func (r *retryStrategy) Backoff(backoffFunc backoff.BackoffFunc) Executor {
+	r.backoffFunc = backoffFunc
+    return r
+}
+
+func (r *retryStrategy) Exec(ctx context.Context, cmd func(ctx context.Context, attempt uint) error) error {
+	var attempt uint
+	for attempt < r.maxAttempts {
+		// execte cmd
+		err := cmd(ctx, attempt)
+		// return if err is nil.
+		if err == nil {
+			return nil
+		}
+		// increase the number of attempts
+		attempt++
+		// sleep and wait retry
+		select {
+		case <-ctx.Done():
+			return ctx.Err()
+		default:
+			time.Sleep(r.backoffFunc(ctx, attempt))
+		}
+	}
+	// perform the last execution
+	return cmd(ctx, attempt)
+}
+
+func MaxAttempts(maxAttempts uint) Retry {
+	return &retryStrategy{
+		maxAttempts: maxAttempts,
+	}	
+}
+
 func Call(ctx context.Context, maxAttempts uint, backoffFunc backoff.BackoffFunc, method func(attemptTime int) error) error {
 	var err error
 	max := int(maxAttempts)
@@ -28,18 +77,4 @@ func Call(ctx context.Context, maxAttempts uint, backoffFunc backoff.BackoffFunc
 		time.Sleep(backoffFunc(ctx, uint(i+1)))
 	}
 	return err
-}
-
-func Endpoint[Req any, Resp any](ctx context.Context, req Req, maxAttempts uint, backoffFunc backoff.BackoffFunc, endpoint func(ctx context.Context, req Req) (Resp, error)) (Resp, error) {
-	for i := uint(0); i < maxAttempts; i++ {
-		resp, err := endpoint(ctx, req)
-		// return if err is nil.
-		if err == nil {
-			return resp, err
-		}
-		// sleep and wait retry
-		time.Sleep(backoffFunc(ctx, i+1))
-	}
-
-	return endpoint(ctx, req)
 }
--- retry/retry_test.go
+++ retry/retry_test.go
@@ -26,3 +26,15 @@ func TestCall(t *testing.T) {
 	err := Call(ctx, uint(maxAttempts), backoffFunc, method)
 	assert.Nil(t, err)
 }
+
+
+func TestRetry(t *testing.T) {
+	err := MaxAttempts(3).Backoff(backoff.Constant(time.Second)).Exec(context.Background(), func(ctx context.Context, attempts uint) error {
+		fmt.Println(attempts)
+		if attempts < 3 {
+			return errors.New("mock error")
+		}
+		return nil
+	})
+	assert.Nil(t, err)
+}
