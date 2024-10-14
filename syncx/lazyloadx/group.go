6f5d057fdeddb9ea70d303b2b20874918289816e
soyacen <soyacen@outlook.com>
2024-10-14 19:26:40 +0800

    feat: modify lazyloadx.Group.Load 方法签名

--- syncx/lazyloadx/group.go
+++ syncx/lazyloadx/group.go
@@ -12,10 +12,10 @@ type Group struct {
 	New func() (any, error)
 }
 
-func (g *Group) Load(key string) (any, bool, error) {
+func (g *Group) Load(key string) (any, error, bool) {
 	// 1. 初次检查,如果 key 已存在于 sync.Map 中，直接返回该值。
 	if value, ok := g.m.Load(key); ok {
-		return value, true, nil
+		return value, nil, true
 	}
 	// 2. 并发控制,g.g.Do 会确保并发控制，即如果有多个 goroutine 同时请求同一个 key，只有一个 goroutine 会执行闭包中的逻辑。
 	value, err, shared := g.g.Do(key, func() (any, error) {
@@ -35,14 +35,14 @@ func (g *Group) Load(key string) (any, bool, error) {
 		return value, nil
 	})
 	if err != nil {
-		return nil, false, err
+		return nil, err, false
 	}
 
 	// 4. 存储结果,如果 shared 为 false，表示当前 goroutine 是唯一执行闭包的 goroutine，此时将结果存储到 sync.Map 中。
 	if !shared {
 		g.m.Store(key, value)
 	}
-	return value, false, err
+	return value, err, false
 }
 
 func (g *Group) Delete(key string) {
