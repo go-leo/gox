550d3e8971cbc04673dff4a0192874b05af0638a
soyacen <soyacen@outlook.com>
2024-10-23 17:54:42 +0800

    feat: add backoff.Random function

---
+++ backoff/random.go
@@ -0,0 +1,15 @@
+package backoff
+
+import (
+	"context"
+	"math/rand"
+	"time"
+)
+
+// Random generates a function that waits for a random time in the range [alpha, beta) between calls.
+func Random(alpha, beta time.Duration) BackoffFunc {
+	r := rand.New(rand.NewSource(time.Now().UnixNano()))
+	return func(ctx context.Context, attempt uint) time.Duration {
+		return alpha + time.Duration(r.Int63n(beta.Nanoseconds()-alpha.Nanoseconds()))
+	}
+}
