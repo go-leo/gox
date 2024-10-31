edebd118f64375104020b7006416719775e89ec0
soyacen <soyacen@outlook.com>
2024-10-31 15:00:07 +0800

    feat: add Unwrap multi err

--- errorx/join.go
+++ errorx/join.go
@@ -6,17 +6,33 @@ func Join(err error, errs ...error) error {
 	if err == nil {
 		return errors.Join(errs...)
 	}
-	// standard join error
-	if joinError, ok := err.(interface{ Unwrap() []error }); ok {
-		return errors.Join(append(joinError.Unwrap(), errs...)...)
-	}
-	// https://github.com/hashicorp/go-multierror/blob/main/multierror.go
-	if multiError, ok := err.(interface{ WrappedErrors() []error }); ok {
-		return errors.Join(append(multiError.WrappedErrors(), errs...)...)
-	}
-	// https://github.com/uber-go/multierr/blob/master/error.go
-	if multiErr, ok := err.(interface{ Errors() []error }); ok {
+	switch multiErr := err.(type) {
+	case interface{ Unwrap() []error }:
+		// standard join error
+		return errors.Join(append(multiErr.Unwrap(), errs...)...)
+	case interface{ WrappedErrors() []error }:
+		// https://github.com/hashicorp/go-multierror/blob/main/multierror.go
+		return errors.Join(append(multiErr.WrappedErrors(), errs...)...)
+	case interface{ Errors() []error }:
+		// https://github.com/uber-go/multierr/blob/master/error.go
 		return errors.Join(append(multiErr.Errors(), errs...)...)
+	default:
+		return errors.Join(append([]error{err}, errs...)...)
 	}
-	return errors.Join(append([]error{err}, errs...)...)
+}
+
+func Unwrap(err error) []error {
+	switch multiErr := err.(type) {
+	case interface{ Unwrap() []error }:
+		// standard join error
+		return multiErr.Unwrap()
+	case interface{ WrappedErrors() []error }:
+		// https://github.com/hashicorp/go-multierror/blob/main/multierror.go
+		return multiErr.WrappedErrors()
+	case interface{ Errors() []error }:
+		// https://github.com/uber-go/multierr/blob/master/error.go
+		return multiErr.Errors()
+	default:
+		return []error{err}
+	}
 }
