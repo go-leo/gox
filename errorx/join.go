79493f0e7c4bedfce274357d0706eb81d3ced664
soyacen <soyacen@outlook.com>
2024-10-31 15:03:11 +0800

    feat: modify join error function

--- errorx/join.go
+++ errorx/join.go
@@ -3,25 +3,13 @@ package errorx
 import "errors"
 
 func Join(err error, errs ...error) error {
-	if err == nil {
-		return errors.Join(errs...)
-	}
-	switch multiErr := err.(type) {
-	case interface{ Unwrap() []error }:
-		// standard join error
-		return errors.Join(append(multiErr.Unwrap(), errs...)...)
-	case interface{ WrappedErrors() []error }:
-		// https://github.com/hashicorp/go-multierror/blob/main/multierror.go
-		return errors.Join(append(multiErr.WrappedErrors(), errs...)...)
-	case interface{ Errors() []error }:
-		// https://github.com/uber-go/multierr/blob/master/error.go
-		return errors.Join(append(multiErr.Errors(), errs...)...)
-	default:
-		return errors.Join(append([]error{err}, errs...)...)
-	}
+	return errors.Join(append(Unwrap(err), errs...)...)
 }
 
 func Unwrap(err error) []error {
+	if err == nil {
+		return nil
+	}
 	switch multiErr := err.(type) {
 	case interface{ Unwrap() []error }:
 		// standard join error
