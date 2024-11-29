2cffd16bdfaaeab6eaab3a8cf4b6f838c4dbbf08
soyacen <soyacen@outlook.com>
2024-11-29 10:45:24 +0800

    feat: Deprecated Merge and Combine

--- syncx/chanx/fan_in.go
+++ syncx/chanx/fan_in.go
@@ -6,10 +6,14 @@ import (
 	"sync"
 )
 
-// FanIn can read from multiple inputs and proceed until all are closed by multiplexing the input
+func FanIn[T any](channels ...<-chan T) <-chan T {
+	return FanInContext[T](context.Background(), channels...)
+}
+
+// FanInContext can read from multiple inputs and proceed until all are closed by multiplexing the input
 // channels onto a single channel that’s closed when all the inputs are closed.
 // See: [concurrency](https://go.dev/talks/2012/concurrency.slide#28)
-func FanIn[T any](ctx context.Context, channels ...<-chan T) <-chan T {
+func FanInContext[T any](ctx context.Context, channels ...<-chan T) <-chan T {
 	out := make(chan T, len(channels))
 	var wg sync.WaitGroup
 	for _, ch := range channels {
@@ -32,7 +36,7 @@ func FanIn[T any](ctx context.Context, channels ...<-chan T) <-chan T {
 	return out
 }
 
-func fanInReflect[T any](channels ...<-chan T) <-chan T {
+func fanIn[T any](channels ...<-chan T) <-chan T {
 	out := make(chan T)
 	go func() {
 		defer close(out)
@@ -59,56 +63,4 @@ func fanInReflect[T any](channels ...<-chan T) <-chan T {
 		}
 	}()
 	return out
-}
-
-func fanInRec[T any](channels ...<-chan T) <-chan T {
-	switch len(channels) {
-	case 0:
-		out := make(chan T)
-		close(out)
-		return out
-	case 1:
-		return channels[0]
-	case 2:
-		return mergeTwo(channels[0], channels[1])
-	default:
-		m := len(channels) / 2
-		return mergeTwo(fanInRec[T](channels[:m]...), fanInRec[T](channels[m:]...))
-	}
-}
-
-func mergeTwo[T any](aChan, bChan <-chan T) <-chan T {
-	out := make(chan T)
-	go func() {
-		defer close(out)
-		for aChan != nil || bChan != nil { //只要还有可读的chan
-			select {
-			case v, ok := <-aChan:
-				if !ok { // aChan 已关闭，设置为nil
-					aChan = nil
-					continue
-				}
-				out <- v
-			case v, ok := <-bChan:
-				if !ok { // bChan 已关闭，设置为nil
-					bChan = nil
-					continue
-				}
-				out <- v
-			}
-		}
-	}()
-	return out
-}
-
-// Merge consolidates multiple input channels into one output channel:
-// Deprecated: Do not use. use FanIn instead.
-func Merge[T any](ctx context.Context, channels ...<-chan T) <-chan T {
-	return FanIn[T](ctx, channels...)
-}
-
-// Combine multiple input channels into one output channel.
-// Deprecated: Do not use. use FanIn instead.
-func Combine[T any](channels ...<-chan T) <-chan T {
-	return FanIn[T](context.Background(), channels...)
 }
