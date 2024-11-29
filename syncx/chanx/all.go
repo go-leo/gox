415e3fcdda682817fad07a8e8608b434e91e6774
soyacen <soyacen@outlook.com>
2024-11-29 10:52:52 +0800

    feat: rename asslice to all

--- syncx/chanx/all.go
+++ syncx/chanx/all.go
@@ -5,8 +5,17 @@ import (
 	"sync"
 )
 
-// All the function collects first value from multiple given channels into a slice and returns it. 
-func All[T any](ctx context.Context, channels ...<-chan T) []T {
+// All 将一个只读通道中的所有元素收集并转换为切片返回。
+func All[T any](c <-chan T) []T {
+	out := make([]T, 0, len(c))
+	for t := range c {
+		out = append(out, t)
+	}
+	return out
+}
+
+// AsyncAll the function collects first value from multiple given channels into a slice and returns it.
+func AsyncAll[T any](ctx context.Context, channels ...<-chan T) []T {
 	if len(channels) <= 0 {
 		return nil
 	}
