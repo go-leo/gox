a38ea346eaf4dd3066b69575edae96da09f6bb0c
soyacen <soyacen@outlook.com>
2024-12-03 11:12:51 +0800

    feat: add chanx.AsSlice

---
+++ syncx/chanx/as_slice.go
@@ -0,0 +1,26 @@
+package chanx
+
+import "context"
+
+// AsSlice 将一个只读通道中的所有元素收集并转换为切片返回。
+func AsSlice[T any](ctx context.Context, in <-chan T) <-chan []T {
+	out := make(chan []T, 1)
+	go func() {
+		defer close(out)
+		s := make([]T, 0)
+		for {
+			select {
+			case <-ctx.Done():
+				out <- s
+				return
+			case t, ok := <-in:
+				if ok {
+					s = append(s, t)
+				}
+				out <- s
+				return
+			}
+		}
+	}()
+	return out
+}
