1af909b13c2774e27ef7b4582d44ef62264cdfe1
soyacen <soyacen@outlook.com>
2024-12-04 20:10:40 +0800

    feat: add chanx.TrySend and TryReceive

--- syncx/chanx/chan.go
+++ syncx/chanx/chan.go
@@ -1,4 +1,33 @@
 package chanx
 
-//func Send[T any](ctx context.Context, in <-chan T, v T) {
-//}
+import (
+	"context"
+	"fmt"
+)
+
+var ErrDefaultBranch = fmt.Errorf("chanx: default branch")
+
+// TrySend 用于尝试将值发送到指定的通道。
+func TrySend[T any](ctx context.Context, in chan<- T, v T) error {
+	select {
+	case in <- v:
+		return nil
+	case <-ctx.Done():
+		return ctx.Err()
+	default:
+		return ErrDefaultBranch
+	}
+}
+
+// TryReceive 用于尝试从通道接收数据。
+func TryReceive[T any](ctx context.Context, in <-chan T) (T, error) {
+	var v T
+	select {
+	case v = <-in:
+		return v, nil
+	case <-ctx.Done():
+		return v, ctx.Err()
+	default:
+		return v, ErrDefaultBranch
+	}
+}
