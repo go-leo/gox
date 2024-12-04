9ece123ab45b1053af3b1b94bcb8a67a5fce72ed
soyacen <soyacen@outlook.com>
2024-12-04 17:35:49 +0800

    feat: remove or done

--- syncx/chanx/pipeline.go
+++ syncx/chanx/pipeline.go
@@ -2,21 +2,29 @@ package chanx
 
 import "context"
 
-// Pipeline creates a data processing pipeline.
-// It takes an input channel `src` and a processing function `f`, returning an output channel `dest`.
-// In a background goroutine, it reads from `src`, applies `f` for processing,
-// and writes the results to `dest`, while listening for cancellation signals from the `ctx` context.
-func Pipeline[T any, R any](ctx context.Context, src <-chan T, f func(T) R) chan<- R {
-	dest := make(chan R)
+// Pipeline 创建一个数据处理管道。
+// 它接收一个输入通道 in 和一个处理函数 f，返回一个输出通道 out。
+// 在新的goroutine中，遍历输入通道中的每个元素，应用处理函数后将结果发送到输出通道。
+// 如果上下文 ctx 被取消，则提前退出并关闭输出通道。
+func Pipeline[T any, R any](ctx context.Context, in <-chan T, f func(T) R) <-chan R {
+	out := make(chan R, len(in))
 	go func() {
-		defer close(dest)
-		for v := range src {
+		defer close(out)
+		for {
 			select {
 			case <-ctx.Done():
 				return
-			case dest <- f(v):
+			case v, ok := <-in:
+				if !ok {
+					return
+				}
+				select {
+				case <-ctx.Done():
+					return
+				case out <- f(v):
+				}
 			}
 		}
 	}()
-	return dest
+	return out
 }
--- syncx/chanx/reduce.go
+++ syncx/chanx/reduce.go
@@ -5,38 +5,9 @@ import (
 )
 
 // Reduce 对输入channel中的元素执行归约操作，使用提供的累积函数和初始值，返回最终结果。
-// 若channel为空，则直接返回初始值。
-func Reduce[T any, R any](in <-chan T, identity R, accumulator func(R, T) R) R {
-	if in == nil {
-		return identity
-	}
-	for value := range in {
+func Reduce[T any, R any](ctx context.Context, in <-chan T, identity R, accumulator func(R, T) R) <-chan R {
+	return Pipeline[T, R](ctx, in, func(value T) R {
 		identity = accumulator(identity, value)
-	}
-	return identity
-}
-
-// AsyncReduce 异步处理通道数据，使用累积器函数累加元素，并能响应上下文取消操作，最终返回累积结果。
-func AsyncReduce[T any, R any](ctx context.Context, in <-chan T, identity R, accumulator func(context.Context, R, T) R) <-chan R {
-	var out chan R
-	if in == nil {
-		return out
-	}
-	out = make(chan R)
-	go func(identity R) {
-		defer close(out)
-		for {
-			select {
-			case <-ctx.Done():
-				return
-			case value, ok := <-in:
-				if !ok {
-					out <- identity
-					return
-				}
-				identity = accumulator(ctx, identity, value)
-			}
-		}
-	}(identity)
-	return out
+		return identity
+	})
 }
