fa6cc8eb36da7f80cfdaef7962a891b5c397c18c
soyacen <soyacen@outlook.com>
2024-12-05 17:12:42 +0800

    feat: remove context from chanx.TrySend and TryReceive

--- syncx/chanx/chan.go
+++ syncx/chanx/chan.go
@@ -1,32 +1,27 @@
 package chanx
 
 import (
-	"context"
 	"fmt"
 )
 
 var ErrDefaultBranch = fmt.Errorf("chanx: default branch")
 
 // TrySend 用于尝试将值发送到指定的通道。
-func TrySend[T any](ctx context.Context, in chan<- T, v T) error {
+func TrySend[T any](in chan<- T, v T) error {
 	select {
 	case in <- v:
 		return nil
-	case <-ctx.Done():
-		return ctx.Err()
 	default:
 		return ErrDefaultBranch
 	}
 }
 
 // TryReceive 用于尝试从通道接收数据。
-func TryReceive[T any](ctx context.Context, in <-chan T) (T, error) {
+func TryReceive[T any](in <-chan T) (T, error) {
 	var v T
 	select {
 	case v = <-in:
 		return v, nil
-	case <-ctx.Done():
-		return v, ctx.Err()
 	default:
 		return v, ErrDefaultBranch
 	}
