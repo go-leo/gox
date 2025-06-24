d3b790c13c2e4322b7adc038fbd4afd8bfdb977f
songyancheng <soyacen@outlook.com>
2025-06-24 14:47:18 +0800

    feat: add slogx method

--- logx/slogx/slog.go
+++ logx/slogx/slog.go
@@ -5,8 +5,10 @@ import (
 	"log/slog"
 )
 
+// attrKey is a private type used as a key for storing attributes in the context.
 type attrKey struct{}
 
+// AppendContext appends attributes to the context.
 func AppendContext(ctx context.Context, attrs ...slog.Attr) context.Context {
 	src, _ := FromContext(ctx)
 	dst := make([]slog.Attr, len(src), len(src)+len(attrs))
@@ -15,23 +17,28 @@ func AppendContext(ctx context.Context, attrs ...slog.Attr) context.Context {
 	return context.WithValue(ctx, attrKey{}, dst)
 }
 
+// NewContext creates a new context with attributes.
 func NewContext(ctx context.Context, attrs ...slog.Attr) context.Context {
 	return context.WithValue(ctx, attrKey{}, attrs)
 }
 
+// FromContext returns the attributes from the context.
 func FromContext(ctx context.Context) ([]slog.Attr, bool) {
 	attr, ok := ctx.Value(attrKey{}).([]slog.Attr)
 	return attr, ok
 }
 
+// WithContextHandler wraps a slog.Handler with context support.
 func WithContextHandler(handler slog.Handler) slog.Handler {
 	return &contextHandler{Handler: handler}
 }
 
+// contextHandler is a private type that implements slog.Handler and adds context support.
 type contextHandler struct {
 	slog.Handler
 }
 
+// Handle processes a log record and adds context attributes if present.
 func (c contextHandler) Handle(ctx context.Context, record slog.Record) error {
 	attrs, ok := FromContext(ctx)
 	if ok {
