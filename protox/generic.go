0557e16cde00a77c4cf950984c73481b9abc7892
soyacen <soyacen@outlook.com>
2025-10-21 15:32:50 +0800

    feat: remove clone file

--- protox/generic.go
+++ protox/generic.go
@@ -2,6 +2,22 @@ package protox
 
 import "google.golang.org/protobuf/proto"
 
+func Clone[M proto.Message](m M) M {
+	return proto.Clone(m).(M)
+}
+
+func CloneSlice[S ~[]M, M proto.Message](s S) S {
+	var zero S
+	if s == nil {
+		return zero
+	}
+	r := make(S, 0, len(s))
+	for _, m := range s {
+		r = append(r, Clone(m))
+	}
+	return r
+}
+
 func MessageSlice[S []E, E proto.Message](s S) []proto.Message {
 	if s == nil {
 		return nil
