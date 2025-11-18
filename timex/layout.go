9f39de1dd3f4a6f37c919597c37d19cd0a0944be
soyacen <soyacen@outlook.com>
2025-11-18 12:04:53 +0800

    feat: add DateHour time layout

--- timex/layout.go
+++ timex/layout.go
@@ -1,19 +1,14 @@
 package timex
 
-import "net/http"
+import (
+	"net/http"
+)
 
-// YearMonth is year month layout
 const YearMonth = "2006-01"
 
-// DateOnlyMonth
-// Deprecated: Do not use. use DateOnlyMonth
-const DateOnlyMonth = YearMonth
+const DateMinute = "2006-01-02 15:04"
 
-// MonthOnly
-// Deprecated: Do not use. use DateOnlyMonth
-const MonthOnly = YearMonth
-
-const DateTimeMinute = "2006-01-02 15:04"
+const DateHour = "2006-01-02 15"
 
 const DateTimeNano = "2006-01-02 15:04:05.999999999"
 
